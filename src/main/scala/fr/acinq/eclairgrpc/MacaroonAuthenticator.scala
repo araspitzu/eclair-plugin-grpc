8e23e18d21fd3523e2b253ae1509725fc95502f1
Andrea Raspitzu <andrea.raspitzu@acinq.fr>
2020-03-06 10:03:46 +0100

    Add macaroon authentication

--- src/main/scala/fr/acinq/eclairgrpc/MacaroonAuthenticator.scala
+++ src/main/scala/fr/acinq/eclairgrpc/MacaroonAuthenticator.scala
@@ -4,29 +4,23 @@ import com.github.nitram509.jmacaroons.{MacaroonsBuilder, MacaroonsVerifier}
 import grizzled.slf4j.Logging
 import io.grpc._
 
-import scala.collection.JavaConverters._
-import scala.util.Try
-
+import scala.util.{Failure, Success, Try}
 
 class MacaroonAuthenticator(adminMacaroon: String) extends ServerInterceptor with Logging {
 
-  def getAsciiMetadata(key: String): Metadata.Key[String] = Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER)
-  val MACAROON_KEY: Metadata.Key[String] = getAsciiMetadata("macaroon")
+  val MACAROON_KEY: Metadata.Key[String] = Metadata.Key.of("macaroon", Metadata.ASCII_STRING_MARSHALLER)
 
   override def interceptCall[ReqT, RespT](serverCall: ServerCall[ReqT, RespT], metadata: Metadata, serverCallHandler: ServerCallHandler[ReqT, RespT]): ServerCall.Listener[ReqT] = {
     Try {
       val clientMacaroon = MacaroonsBuilder.deserialize(metadata.get(MACAROON_KEY))
       val verifier = new MacaroonsVerifier(clientMacaroon)
-      logger.info(s"clientMacaroon: ${clientMacaroon.inspect()}")
-      logger.info(s"macaroon okay: ${verifier.isValid(adminMacaroon)}")
+      verifier.isValid(adminMacaroon)
+    } match {
+      case Success(_) =>
+        serverCallHandler.startCall(serverCall, metadata)
+      case Failure(ex) =>
+        logger.error("Macaroon failed:", ex)
+        throw ex
     }
-
-    logger.info(s"METADATA:")
-    metadata.keys().asScala.foreach { key =>
-      logger.info(s"$key: ${metadata.get(getAsciiMetadata(key))}")
-    }
-
-    // continue
-    serverCallHandler.startCall(serverCall, metadata)
   }
 }
